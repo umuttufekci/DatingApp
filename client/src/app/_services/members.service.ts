970e83e23b6c59ef7cb326b1c7cd5e1e84ea60e3
umutt <umittfkc@gmail.com>
2021-03-22 23:00:15 +0100

    End of section 9

---
+++ client/src/app/_services/members.service.ts
@@ -0,0 +1,23 @@
+import { environment } from './../../environments/environment';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Member } from '../_models/member';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getMembers() {
+    return this.http.get<Member[]>(this.baseUrl + 'users');
+  }
+
+  getMember(username: string) {
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
+  }
+
+}
